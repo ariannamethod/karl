6fa3b05e3e37bb8e25703d6eb3bf0dd5a4a0d5b9
Oleg <theariannamethod@gmail.com>
2025-07-10 07:28:07 +0300

    Load env variables automatically

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ from aiogram.utils.chat_action import ChatActionSender
 from openai import OpenAI
 from openai.assistants import Assistants
 
+from dotenv import load_dotenv
 from utils.memory import MemoryManager
 from utils.tools import split_message
+
+load_dotenv()
 
 # --- Config ---
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
