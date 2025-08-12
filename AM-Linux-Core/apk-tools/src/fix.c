f699fcdff78c31ab08f0bfd2d86df862018d78cc
Arianna Method <theariannamethod@gmail.com>
2025-08-12 06:26:08 +0300

    Update fix.c

--- AM-Linux-Core/apk-tools/src/fix.c
+++ AM-Linux-Core/apk-tools/src/fix.c
@@ -1,14 +1,3 @@
-/* fix.c - Alpine Package Keeper (APK)
- *
- * Copyright (C) 2005-2008 Natanael Copa <n@tanael.org>
- * Copyright (C) 2008 Timo Teräs <timo.teras@iki.fi>
- * All rights reserved.
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 as published
- * by the Free Software Foundation. See http://www.gnu.org/ for details.
- */
-
 #include <errno.h>
 #include <stdio.h>
 #include "apk_applet.h"
